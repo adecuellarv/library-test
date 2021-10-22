47e337bbf2491a5b4033e5032078a6ee437c56d6
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2021-10-21 23:43:39 -0500

    se agrega angular material y se crea header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LibraryCatalogueComponent } from './components/library-catalogue/library-catalogue.component';
-import { BookComponent } from './components/book/book.component';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { MatButtonModule } from '@angular/material/button'
+//import { MatRaisedButtonModule } from '@angular/material/raised'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './components/header/header.component'
+import { LibraryCatalogueComponent } from './components/library-catalogue/library-catalogue.component'
+import { BookComponent } from './components/book/book.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -16,9 +18,11 @@ import { BookComponent } from './components/book/book.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
